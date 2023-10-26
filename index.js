4b5ff93521ec4d386b6c6e2f1e61dff73adc717f
dnlstm <dnjstm0546@naver.com>
2023-10-26 11:25:21 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// const bodyParser = require("body-parser");
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(bodyParser.join());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://alfnskfk:alfnvv!2@cluster0.ujagqye.mongodb.net/?retryWrites=true&w=majority",
-    {
-      // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAnModify: false,
-    }
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("MongoDB Connected..."))
   .catch((arr) => console.log(arr));
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
+});
+
+app.post("/register ", async (req, res) => {
+  const user = new User(req, body);
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        success: false,
+        err: err,
+      });
+    });
 });
 
 app.listen(port, () => {
